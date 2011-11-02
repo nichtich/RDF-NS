d0a2d89428667b991720e1e2d8f1fa8fa5605cfd
Jakob Voss <jakob@nichtich.de>
2011-11-02 22:03:28 +0100

    updated 20111102 with CLI

--- lib/RDF/NS.pm
+++ lib/RDF/NS.pm
@@ -75,7 +75,6 @@ sub TXT {
     $self->MAP( sub { "$_\t".$self->{$_} } , @_ );
 }
 
-
 sub SELECT {
     my $self = shift;
     $self->MAP( sub { $_ => $self->{$_} } , @_ );
@@ -121,8 +120,8 @@ sub AUTOLOAD {
 
 =head1 SYNOPSIS
 
-  use RDF::NS '20111031';              # check at compile time
-  my $ns = RDF::NS->new('20111031');   # check at runtime
+  use RDF::NS '20111102';              # check at compile time
+  my $ns = RDF::NS->new('20111102');   # check at runtime
 
   $ns->foaf;               # http://xmlns.com/foaf/0.1/
   $ns->foaf_Person;        # http://xmlns.com/foaf/0.1/Person
@@ -135,7 +134,7 @@ sub AUTOLOAD {
 
   # To get RDF::Trine::Node::Resource instead of strings
   use RDF::NS::Trine;
-  $ns = RDF::NS::Trine->new('20111031');
+  $ns = RDF::NS::Trine->new('20111102');
   $ns->foaf_Person;        # iri('http://xmlns.com/foaf/0.1/Person')
 
   # load your own mapping
@@ -159,11 +158,17 @@ defined at specific snapshots in time. These snapshots correspond to version
 numbers of this module. By selecting particular versions, you make sure that
 changes at prefix.cc won't affect your scripts.
 
-This module does not require L<RDF::Trine> which is recommended nevertheless.
+The command line client L<rdfns> is installed automatically with this module:
+
+  $ rdfns -ttl rdf,foaf
+  @prefix foaf: <http://xmlns.com/foaf/0.1/> .
+  @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+
+This module does not require L<RDF::Trine>, which is recommended nevertheless.
 If you prefer RDF::NS to return instances of L<RDF::Trine::Node::Resource>
 instead of plain strings, use L<RDF::NS::Trine>.
 
-The code repository of this module contains an
+The code repository of this module also contains an
 L<update script|https://github.com/nichtich/RDF-NS/blob/master/update.pl>
 to download the current prefix-namespace mappings from L<http://prefix.cc>.
 
@@ -202,6 +207,10 @@ vertical bars, and spaces.
 Returns an XML namespace declaration or a list of such declarations in list
 context. Prefixes can be passed as single arguments or separated by commas,
 vertical bars, and spaces.
+
+=method TXT ( prefix[es] )
+
+Returns a list of tabular-separated prefix-namespace-mappings.
 
 =method SELECT ( prefix[es] )
 
